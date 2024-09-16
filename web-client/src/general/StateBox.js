8438d2f9435dce8635366b8ec119857cdf363466
JonathanKelsi <109662660+JonathanKelsi@users.noreply.github.com>
2024-09-16 11:52:47 +0300

    Update StateBox.js
    Bro learn about dictionaries

--- web-client/src/general/StateBox.js
+++ web-client/src/general/StateBox.js
@@ -4,25 +4,22 @@ import { React, useRef } from "react";
 import { getReformattedDate } from "../../../shared/userApi.js";
 
 function StateBox({ state }) {
-
-
-        if (state === "FAILED") {
-            return (
-                <button type="button" class="btn btn-danger disable"></button>
-            );
-        } else if (state === "PASSED") {
-            return (
-                <button type="button" class="btn btn-success disable"></button>
-            );
-        } else if (state === "UNTESTED") {
-            return (
-                <button type="button" class="btn btn-secondary disable"></button>
-            ); 
-        } else {
-            return (
-                <button type="button" class="btn btn-warning disable"></button>
-            ); 
+        state_to_style = {
+                "FAILED": "btn-danger",
+                "PASSED": "btn-success",
+                "UNTESTED": "btn-secondary",
         }
+
+        if (state in state_to_style) {
+                return (
+                        <button type="button" class=`btn ${state_to_style[state]} disable`></button>
+                );
+        } 
+
+        // If state not defined - return warning
+        return (
+                <button type="button" class="btn btn-warning disable"></button>
+        );
 }
 
 export default StateBox;
