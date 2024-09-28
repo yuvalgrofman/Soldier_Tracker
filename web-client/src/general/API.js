3875660e6f3685c2a6c3fc597aef4ae5647cbd46
Yuval Grofman <yuvalgrofman@gmail.com>
2024-09-28 19:08:50 +0300

    added fetches and db

--- web-client/src/general/API.js
+++ web-client/src/general/API.js
@@ -2,7 +2,7 @@
 // import img from '../images/placeholder.png'
 import img from '../images/image1.jpeg'
 
-
+//  ------------------------------------------------------------------------------------------------
 
 // All objectId fields are for testing, and are automatically created in MongoDB.
 let s1 = { citizenshipID: 1 , objectId: 1 , armyID: 1 , name: "Yuval", section: "Sec 1", platoon: "Plat 1", company: "Yud", profilePic: img}
@@ -27,7 +27,6 @@ let r13 = { objectId: 104, testName: "Test 1", soldierID: 3, score: 50, isComple
 
 let r21 = { objectId: 102, testName: "Test 2", soldierID: 1, score: 95, isCompleted: true }
 
-
 let test1 = {
     name: "TestOne",
     type: "HIGH",
@@ -36,7 +35,77 @@ let test1 = {
     results: [ 101, 103, 104 ] // Real objectIds aren't numbers, it's for testing...
 }
 
+// ------------------------------------------------------------------------------------------------
 
+let sect1 = { name: "Section 1", soldierIds: [1, 2], commander: "MC 1" }
+let sect2 = { name: "Section 2", soldierIds: [3, 4], commander: "MC 2" }
+let sect3 = { name: "Section 3", soldierIds: [5, 6], commander: "MC 3" }
+let sect4 = { name: "Section 4", soldierIds: [7, 8], commander: "MC 4" }
+let sect5 = { name: "Section 5", soldierIds: [9, 10], commander: "MC 5" }
+let sect6 = { name: "Section 6", soldierIds: [11, 12], commander: "MC 6" }
+
+let plat1 = { name: "Platoon 1", sectionIds: [1, 2], commander: "MM 1" }
+let plat2 = { name: "Platoon 2", sectionIds: [3, 4], commander: "MM 2" }
+let plat3 = { name: "Platoon 3", sectionIds: [5, 6], commander: "MM 3" }
+
+let comp1 = { name: "Company 1", platoonIds: [1, 2], commander: "MC 1" }
+
+//  ------------------------------------------------------------------------------------------------
+
+let testDisplay1 = { name: "10 Meter Range", link: "10MeterRange" }
+let testDisplay2 = { name: "25 Meter Range", link: "25MeterRange" }
+let testDisplay3 = { name: "50 Meter Range", link: "50MeterRange" }
+
+let testDisplay4 = { name: "Shooting Positions", link: "ShootingPositions" }
+let testDisplay5 = { name: "Shooting & Walking", link: "ShootingWalking" }
+let testDisplay6 = { name: "Shooting At Night", link: "ShootingAtNight" }
+
+let testDisplay7 = { name: "Running", link: "Running" }
+let testDisplay8 = { name: "Sprints", link: "Sprints" }
+let testDisplay9 = { name: "Obstacle Course", link: "ObstacleCourse" }
+
+let testDisplay10 = { name: "Clearing Buildings", link: "ClearingBuildings" }
+let testDisplay11 = { name: "Room Clearing", link: "RoomClearing" }
+let testDisplay12 = { name: "Urban Combat", link: "UrbanCombat" }
+
+let testDisplay13 = { name: "Trench Warfare", link: "TrenchWarfare" }
+let testDisplay14 = { name: "Open Field Combat", link: "OpenFieldCombat" }
+let testDisplay15 = { name: "Warfare In Forests", link: "WarfareInForests" }
+
+let contentDisplay1 = { name: "Shooting 1", link: "ShootingOne", tests: [testDisplay1, testDisplay2, testDisplay3] }
+let contentDisplay2 = { name: "Shooting 2", link: "ShootingTwo", tests: [testDisplay4, testDisplay5, testDisplay6] }
+let contentDisplay3 = { name: "Physical Exams", link: "Physical", tests: [testDisplay7, testDisplay8, testDisplay9] }
+let contentDisplay4 = { name: "Urban Warfare", link: "Urban", tests: [testDisplay10, testDisplay11, testDisplay12] }
+let contentDisplay5 = { name: "Open Warfare", link: "Open", tests: [testDisplay13, testDisplay14, testDisplay15] }
+
+const contents = [contentDisplay1, contentDisplay2, contentDisplay3, contentDisplay4, contentDisplay5]
+
+// ------------------------------------------------------------------------------------------------
+
+let settingsDisplay1 = { name: "Add Soldier", link: "AddSoldier" }
+let settingsDisplay2 = { name: "Add User", link: "AddUser" }
+// let settingsDisplay3 = { name: "Add Section", link: "AddSection" }
+
+let settingDisplay = { name: "Settings", link: "Settings", tests: [ settingsDisplay1, settingsDisplay2 ] }
+
+//------------------------------------------------------------------------------------------------
+// FETCH:
+
+function fetchSettingDisplays() {
+    return settingDisplay;
+}
+
+function fetchContentDisplays() {
+    return contents;
+}
+
+function fetchContentDisplay(contentLink) {
+    return contents.find(c => c.link === contentLink);
+}
+
+function fetchTestDisplay(contentLink, testLink) {
+    return contents.find(c => c.link === contentLink).tests.find(t => t.link === testLink);
+}
 
 async function fetchTest(testName) {
     return test1;
@@ -49,22 +118,8 @@ async function fetchTest(testName) {
     // return JSON.parse(await res.text());
 }
 
-
 async function fetchTestResults(test) {
     return [r11, r12, r13];
-}
-
-// async function fetchSoldiers() {
-//     return [s1, s2, s3];
-// }
-
-async function postSoldierToSection(sectionID, soldierID) {
-    // const res = await fetch("http://127.0.0.1:5022/api/Force/addSoldier/", {
-    //     method: "POST",
-    //     body: JSON.stringify({ soldierID, sectionID }),
-    // });
-    
-    // if (!res.ok) return null;
 }
 
 async function fetchSoldier(armyID) {
@@ -95,6 +150,32 @@ async function fetchSoldiers(armyIDList) {
     return res
 }
 
+async function fetchSectionSoldiers(sectionID) {
+    let sectionMap = {
+        1: [s1, s2],
+        2: [s3, s4],
+        3: [s5, s6],
+        4: [s7, s8],
+        5: [s9, s10],
+        6: [s11, s12],
+    }
+    return sectionMap[sectionID];
+} 
+
+async function fetchPlatoonSoldiers(platoonID) {
+    let platoonMap = {
+        1: [s1, s2, s3, s4],
+        2: [s5, s6, s7, s8],
+        3: [s9, s10, s11, s12],
+    }
+    return platoonMap[platoonID];
+}
+
+async function fetchCompanySoldiers(companyID) {
+    return [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12]
+}
+
+
 async function fetchResultByTestAndSoldier(testName, soldierID) {
     // const res = await fetch("http://127.0.0.1:5022/api/Results/" + testName + "-" + soldierID, {
     //     method: "GET",
@@ -104,6 +185,22 @@ async function fetchResultByTestAndSoldier(testName, soldierID) {
     //
     // return JSON.parse(await res.text());
     return r11
+}
+
+async function fetchContentTest(contentName) {
+    return 
+}
+
+// ------------------------------------------------------------------------------------------------
+// POST:
+
+async function postSoldierToSection(sectionID, soldierID) {
+    // const res = await fetch("http://127.0.0.1:5022/api/Force/addSoldier/", {
+    //     method: "POST",
+    //     body: JSON.stringify({ soldierID, sectionID }),
+    // });
+    
+    // if (!res.ok) return null;
 }
 
 async function postPlatoon(platoon) {
@@ -202,6 +299,8 @@ async function postVerifyUser(username, password) {
 
 export {
     fetchTest, fetchSoldier, fetchResultByTestAndSoldier, fetchTestResults, fetchSoldiers,
+    fetchContentDisplays, fetchContentDisplay, fetchTestDisplay, fetchContentTest, fetchSettingDisplays,
+    fetchCompanySoldiers, fetchPlatoonSoldiers, fetchSectionSoldiers,
     postSoldier, postUser, postResult, postSection, postPlatoon, postCompany,
     postSoldierToSection, postVerifyUser, 
     // For testing:
