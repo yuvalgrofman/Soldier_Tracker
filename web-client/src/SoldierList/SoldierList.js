71f671b9b9da39b3cce102f6454c206f15f7ed69
Yuval Grofman <yuvalgrofman@gmail.com>
2024-09-28 19:09:43 +0300

    Made soldier list support force filter

--- web-client/src/SoldierList/SoldierList.js
+++ web-client/src/SoldierList/SoldierList.js
@@ -1,8 +1,27 @@
 /** @format */
 
 import SoldierListElement from "./SoldierListElement";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { fetchSectionSoldiers, fetchPlatoonSoldiers, fetchCompanySoldiers } from "../general/API";
 
-function SoldierList({ soldiers }) {
+function SoldierList() {
+    const { force, id } = useParams();
+
+    const [soldiers, setSoldiers] = useState([]);
+
+    const fetchForceMap = {
+        "Section" : fetchSectionSoldiers,
+        "Platoon": fetchPlatoonSoldiers,
+        "Company": fetchCompanySoldiers
+    }
+
+    useEffect(() => {
+        fetchForceMap[force](id).then((soldiers) => {
+            setSoldiers(soldiers);
+        });
+    }, [force, id]);
+
     
     const soldierComponents = soldiers.map((soldier) => (
         <SoldierListElement
